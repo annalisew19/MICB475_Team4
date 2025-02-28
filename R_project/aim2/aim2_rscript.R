c6c6a87d4b0aa7ce43a497d6da683f3dd75f0485
michelleyang04 <michelleyang121212@gmail.com>
2025-02-27 19:25:21 -0800

    Update aim2_rscript.R

--- R_project/aim2/aim2_rscript.R
+++ R_project/aim2/aim2_rscript.R
@@ -12,7 +12,7 @@ library(ape)
 IVFmeta <- read_delim("IVF_metadata.tsv", delim = "\t")
 
 # select for coloumns, include only sample id, age, prg outcome (disease), sample name, and tissue
-IVFmeta_select <- select(IVFmeta, `sample-id`,`AGE`, `disease`, `Sample Name`, `tissue`)
+IVFmeta_select <- select(IVFmeta, `sample-id`,`AGE`, `disease`, `tissue`)
 
 # filters rows that have NA in the column AGE and disease
 IVFmeta_filter <- filter(IVFmeta_select, 
@@ -58,4 +58,40 @@ IVFmeta_age_and_outcome %>%
 IVFotu <- read_delim("feature-table.txt", delim = "\t", skip=1)
 IVFtax <- read_delim("taxonomy.tsv", delim="\t")
 IVFphylotree <- read.tree("tree.nwk")
+
+### format OTU table
+# save everything except first column (OTU ID) into a matrix
+ivf_otu_mat <- as.matrix(IVFotu[,-1]) #remove the first index column
+# Make first column (#OTU ID) the rownames of the new matrix
+rownames(ivf_otu_mat) <- IVFotu$`#OTU ID`
+# Use the "otu_table" function to make an OTU table
+ivf_OTU <- otu_table(ivf_otu_mat, taxa_are_rows = TRUE) 
+class(ivf_OTU)
+
+### Format sample metadata
+# Save everything except sampleid as new data frame
+ivf_samp_df <- as.data.frame(IVFmeta_age_and_outcome[,-1])
+# Make sampleids the rownames
+rownames(ivf_samp_df)<- IVFmeta_age_and_outcome$'sample-id'
+# Make phyloseq sample data with sample_data() function
+ivf_SAMP <- sample_data(ivf_samp_df)
+class(ivf_SAMP)
+
+### Format taxonomy
+# Convert taxon strings to a table with separate taxa rank columns
+ivf_tax_mat <- IVFtax %>% select(-Confidence)%>% #remove Confidence columm
+  separate(col=Taxon, sep="; "
+           , into = c("Domain","Phylum","Class","Order","Family","Genus","Species")) %>%
+  as.matrix() # Saving as a matrix
+# Save everything except feature IDs 
+ivf_tax_mat <- ivf_tax_mat[,-1]
+# Make sampleids the rownames
+rownames(ivf_tax_mat) <- IVFtax$`Feature ID`
+# Make taxa table
+ivf_TAX <- tax_table(ivf_tax_mat)
+class(ivf_TAX)
+
+### Create phyloseq object 
+# Merge all into a phyloseq object
+ivf_mpt <- phyloseq(ivf_OTU, ivf_SAMP, ivf_TAX, IVFphylotree)
 
