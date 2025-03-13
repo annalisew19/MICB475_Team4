91b0b5bbfa406c451db956f668aa3417ff0a34f2
michelleyang04 <michelleyang121212@gmail.com>
2025-03-12 20:50:15 -0700

    Update aim2_core_microbiome_script.R

--- R_project/aim2/aim2_core_microbiome_script.R
+++ R_project/aim2/aim2_core_microbiome_script.R
@@ -9,31 +9,34 @@ load("ivf_phyloseq.RData") #unrarafied object, analysis take vary seq depth into
 
 #### "core" microbiome ####
 
-# Convert to relative abundance
+# Convert raw read counts to relative abundance. counts -> percentage, so more comparable
 phyloseq_RA <- transform_sample_counts(ivf_phyloseq, fun=function(x) x/sum(x))
 
 # Filter dataset by outcome
 phyloseq_successful_outcome <- subset_samples(phyloseq_RA, `outcome`=="successful")
 phyloseq_unsuccesful_outcome <- subset_samples(phyloseq_RA, `outcome`=="unsuccessful")
 
-# What ASVs are found in more than 70% of samples in each outcome category?
-# trying changing the prevalence to see what happens
+# core microbiome calculation
+  # detection: ASV must have relative abundance greater than 0; must be present to be considered
+  # prevalence: ASV must be present in at least xx% of the sample
+  #tried that the higehst prevalence is 0.4
 successful_ASVs <- core_members(phyloseq_successful_outcome, detection=0, prevalence = 0.4)
 unsuccessful_ASVs <- core_members(phyloseq_unsuccesful_outcome, detection=0, prevalence = 0.4)
 
-# What are these ASVs? you can code it in two different ways to see the same things
-succ_table <- tax_table(prune_taxa(successful_ASVs, ivf_phyloseq))
-unsucc_table <- tax_table(prune_taxa(unsuccessful_ASVs, ivf_phyloseq))
+# retrieve taxonomic classifications of the ASVs identified as core members
+tax_table(prune_taxa(successful_ASVs, ivf_phyloseq))
+tax_table(prune_taxa(unsuccessful_ASVs, ivf_phyloseq))
 
 
 # can plot those ASVs' relative abundance
 prune_taxa(unsuccessful_ASVs,phyloseq_RA) %>% 
-  plot_bar(fill="Genus") + 
-  facet_wrap(.~`outcome`, scales ="free") #x-axis don't depend on each other
+  plot_bar(fill="Genus")
+#  facet_wrap(.~`outcome`, scales ="free") #x-axis don't depend on each other
 #less groups from antibiotic group
 
 # Notice that in this dataset, there are very few CORE microbiome members. This is common
 ### What if we wanted to make a Venn diagram of all the ASVs that showed up in each treatment?
+ #detection more tha 0.001 (0.1%)
 successful_list <- core_members(phyloseq_successful_outcome, detection=0.001, prevalence = 0.10)
 unsuccessful_list <- core_members(phyloseq_unsuccesful_outcome, detection=0.001, prevalence = 0.10)
 
@@ -43,4 +46,21 @@ outcome_list_full <- list(Successful = successful_list, Unsuccessful = unsuccess
 # Create a Venn diagram using all the ASVs shared and unique to antibiotic users and non users
 first_venn <- ggVennDiagram(x = outcome_list_full)
 
-ggsave("venn_antibiotic.png", first_venn)
+ggsave("venn_outcome", first_venn)
+
+
+# Get the taxonomy table from your original phyloseq object
+taxonomy <- as.data.frame(tax_table(ivf_phyloseq))
+
+# Add ASV names as rownames (makes subsetting easier)
+taxonomy$ASV <- rownames(taxonomy)
+
+# Get unique ASVs for successful outcome
+unique_successful_ASVs <- setdiff(successful_list, unsuccessful_list)
+
+# Get unique ASVs for unsuccessful outcome
+unique_unsuccessful_ASVs <- setdiff(unsuccessful_list, successful_list)
+
+# Create tables of unique ASVs and their taxonomy
+successful_taxa <- taxonomy[taxonomy$ASV %in% unique_successful_ASVs, ]
+unsuccessful_taxa <- taxonomy[taxonomy$ASV %in% unique_unsuccessful_ASVs, ]
